dc3ce3aca76ddf2021dafb2ac944794f63b88cc1
Andrew Jones <ajon542@gmail.com>
2015-06-12 09:03:46 -0700

    Initial commit for max subarray algorithm

---
+++ MaximumSubArray/README.md
@@ -0,0 +1,8 @@
+﻿# MaximumSubArray
+Implementation of the maximum subarray problem algorithm presented in the MIT "Introduction to Algorithms" course. This is more or less a test bed for the exercises and pseudocode in the book.
+
+# Notes and references
+* http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/
+* Cormen, T. (2009). Introduction to algorithms (3rd ed.). Cambridge, Mass.: MIT Press.
+
+
---
+++ MaximumSubArray/app.js
@@ -0,0 +1,63 @@
+﻿// Input: the array A and the indices low, mid, and high.
+// Returns: a tuple containing the indices demarcating a maximum subarray that
+// crosses the midpoint, along with the sum of the values in a maximum subarray.
+function FindMaxCrossingSubArray(arr, low, mid, high) {
+    
+    // Find a maximum subarray of the left half.
+    // Must contain 'mid'.
+    var sum = 0;    
+    var leftSum = -Infinity;
+    var maxLeft;
+    
+    for (var i = mid; i >= low; --i) {
+        sum += arr[i];
+        if (sum > leftSum) {
+            leftSum = sum;
+            maxLeft = i;
+        }
+    }
+    
+    // Find a maximum subarray of the right half.
+    // Must contain 'mid + 1'.
+    sum = 0;    
+    var rightSum = -Infinity;
+    var maxRight;
+
+    for (var i = mid + 1; i <= high; ++i) {
+        sum += arr[i];
+        if (sum > rightSum) {
+            rightSum = sum;
+            maxRight = i;
+        }
+    }
+
+    return { low: maxLeft, high: maxRight, sum: leftSum + rightSum };
+}
+
+// Input: the array A and the indices low and high.
+// Returns: a tuple containing the indices demarcating a maximum subarray
+// along with the sum of the values in a maximum subarray.
+function FindMaximumSubArray(arr, low, high) {
+    if (high == low) {
+        return { low: low, high: high, sum: arr[low] };
+    } else {
+        var mid = Math.floor((low + high) / 2);
+        var left = FindMaximumSubArray(arr, low, mid);
+        var right = FindMaximumSubArray(arr, mid + 1, high);
+        var cross = FindMaxCrossingSubArray(arr, low, mid, high);
+
+        if ((left.sum >= right.sum) && (left.sum >= cross.sum)) {
+            return { low: left.low, high: left.high, sum: left.sum };
+        } else if ((right.sum >= left.sum) && (right.sum >= cross.sum)) {
+            return { low: right.low, high: right.high, sum: right.sum };
+        } else {
+            return { low: cross.low, high: cross.high, sum: cross.sum };
+        }
+    }
+}
+
+var arr = [ 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 ];
+var result = FindMaximumSubArray(arr, 0, 15);
+console.log(result.low);
+console.log(result.high);
+console.log(result.sum);
